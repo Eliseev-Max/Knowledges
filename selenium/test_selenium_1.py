5962e2cc7afd6aed3c4a1b9b89bf9ce6275e867e
Eliseev-Max <m.eliseev@prosoftsystems.ru>
2021-06-11 18:53:37 +0500

    Дописал конспект по ожиданию, попробовал код selenium

--- selenium/test_selenium_1.py
+++ selenium/test_selenium_1.py
@@ -1,8 +1,17 @@
 import pytest
+import selenium
 
 
-@pytest.mark.parametrize("url, title",[("https://yandex.ru", "Яндекс"),("https://google.com", "Google")])
-def test_title_yandex(browser, url, title):
-    browser.get(url)
-    current_title = browser.title
-    assert current_title == title
+# @pytest.mark.parametrize("url, title",[
+#     ("http://172.19.16.229/", "Your Store"),
+#     ("https://yandex.ru", "Яндекс"),
+#     ("https://google.com", "Google")
+# ])
+# def test_title_yandex(browser, url, title):
+#     browser.get(url)
+#     current_title = browser.title
+#     assert current_title == title
+
+def test_base_url(browser, base_url):
+    browser.get(base_url)
+    assert browser.find_element_by_css_selector("#logo")
