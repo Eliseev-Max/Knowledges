2b9f88dbeb27c6fe2d188243e259e0fd610d72a3
Eliseev-Max <yeliseyev.max@gmail.com>
2021-06-21 23:09:39 +0500

    Намётки для ДЗ

--- selenium/test_waiting.py
+++ selenium/test_waiting.py
@@ -7,7 +7,6 @@ def test_has_title_appeared(browser, base_url):
     wait_title(TITLE, browser, timeout=1)
 
 def test_featured_links(browser, base_url):
-    #APPEARING_ELEMENT = r".dropdown-menu .text-center"
     browser.get(base_url)
     browser.find_element(By.CSS_SELECTOR, "#cart button").click()
     timeout_handle(browser, "p.text-center")
@@ -18,3 +17,17 @@ def test_title_became_Search(browser, base_url):
     browser.get(base_url)
     browser.find_element(By.CSS_SELECTOR, ".input-group-btn button").click()
     wait_title(TITLE, browser, timeout=1)
+
+
+# Что ещё проверить?
+# <i class="fa fa-heart"></i> - нажать на сердечко, Появится надпись
+#   You must login or create an account to save MacBook to your wish list!
+# Нажать на стрелочки ("Compare this product") - появится надпись
+#  Success: You have added MacBook to your product comparison!
+# Свипперы <div class="swiper-pagination carousel0 swiper-pagination-clickable swiper-pagination-bullets">
+#   <span class="swiper-pagination-bullet swiper-pagination-bullet-active"></span>
+# Каталог: нажатие List/ Grid
+#   Sort by
+#
+#
+#
