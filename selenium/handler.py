b1869ee0023f5f18dc86ea475cb2e4528b17b29c
Eliseev-Max <m.eliseev@prosoftsystems.ru>
2021-06-22 19:41:53 +0500

    Начал конспект лекции по PO

--- selenium/handler.py
+++ selenium/handler.py
@@ -14,15 +14,31 @@ def element_existence(driver, url, tuple_By_element):
         raise AssertionError(err)
 
 
-def wait_title(title, driver, timeout=3):
+def wait_title(driver, url, title, click = False, timeout=3):
+    driver.get(url)
     try:
+        if click is not False:
+            driver.find_element(*click).click()
         WebDriverWait(driver, timeout).until(EC.title_is(title))
+    except NoSuchElementException as no_elem:
+        raise AssertionError(no_elem, "Данный селектор не обнаружен")
     except TimeoutException:
         raise AssertionError("title не успел подгрузиться")
 
 
-def timeout_handle(driver, selector, timeout=3):
+def handle_timeout(driver, LOCATOR, timeout=3):
     try:
-        WebDriverWait(driver, timeout).until(EC.visibility_of_element_located((By.CSS_SELECTOR, selector)))
+        WebDriverWait(driver, timeout).until(EC.visibility_of_element_located(LOCATOR))
     except TimeoutException:
-        raise AssertionError("Селектор {} не обнаружен".format(selector))
+        raise AssertionError("Веб-элемент {} не обнаружен".format(LOCATOR[1]))
+
+
+def find_element_after_click(driver, url, elem_to_click, required_locator, timeout=2):
+    driver.get(url)
+    try:
+        driver.find_element(*elem_to_click).click()
+        WebDriverWait(driver, timeout).until(EC.visibility_of_element_located(required_locator))
+    except NoSuchElementException as no_elem:
+        raise AssertionError(no_elem, "Данный селектор не обнаружен")
+    except TimeoutException as time_is_up:
+        raise AssertionError(time_is_up, "Время ожидания элемента истекло")
--- selenium/test_waiting.py
+++ selenium/test_waiting.py
@@ -1,33 +1,45 @@
 from handler import *
+from WebPages import *
 
 
+LAPTOPS_AND_NOTEBOOKS = "laptop-notebook/"
+PRODUCT_CARD = "mp3-players/ipod-classic"
+LOGIN_PAGE = "index.php?route=account/login"
+ADMIN_LOGIN_PAGE = "admin/"
+
+
+# Поиск элементов на Главной странице
 def test_has_title_appeared(browser, base_url):
     TITLE = "Your Store"
-    browser.get(base_url)
-    wait_title(TITLE, browser, timeout=1)
-
-def test_featured_links(browser, base_url):
-    browser.get(base_url)
-    browser.find_element(By.CSS_SELECTOR, "#cart button").click()
-    timeout_handle(browser, "p.text-center")
+    wait_title(browser, base_url, TITLE, timeout=2)
 
 
-def test_title_became_Search(browser, base_url):
+def test_click_shopping_cart_button(browser, base_url):
+    find_element_after_click(browser, base_url, MainPage.CART_BUTTON, MainPage.SHOPPING_CART_ALERT)
+
+
+def test_make_title_Search(browser, base_url):
     TITLE = "Search"
-    browser.get(base_url)
-    browser.find_element(By.CSS_SELECTOR, ".input-group-btn button").click()
-    wait_title(TITLE, browser, timeout=1)
+    wait_title(browser, base_url, TITLE, click=MainPage.SEARCH_BUTTON)
 
 
-# Что ещё проверить?
-# <i class="fa fa-heart"></i> - нажать на сердечко, Появится надпись
-#   You must login or create an account to save MacBook to your wish list!
-# Нажать на стрелочки ("Compare this product") - появится надпись
-#  Success: You have added MacBook to your product comparison!
-# Свипперы <div class="swiper-pagination carousel0 swiper-pagination-clickable swiper-pagination-bullets">
-#   <span class="swiper-pagination-bullet swiper-pagination-bullet-active"></span>
-# Каталог: нажатие List/ Grid
-#   Sort by
-#
-#
-#
+def test_message_adding_in_wish_list(browser, base_url):
+    find_element_after_click(browser, base_url, MainPage.ADD_TO_WISH_LIST, MainPage.SUCCESS_OR_DISMISS_ALERT)
+
+
+def test_My_Account_drop_down_list(browser, base_url):
+    find_element_after_click(browser, base_url, MainPage.MY_ACCOUNT_CARET, MainPage.REGISTER, timeout=0.5)
+    find_element_after_click(browser, base_url, MainPage.MY_ACCOUNT_CARET, MainPage.LOGIN, timeout=0.5)
+
+
+# Laptops & Notebooks
+
+
+def test_add_product_to_comparison(browser, base_url):
+    URL = base_url + LAPTOPS_AND_NOTEBOOKS
+    find_element_after_click(browser, URL, Catalog.COMPARE_PRODUCT, Catalog.PRODUCT_COMPARE_1)
+
+
+def test_click_Login_without_filling_fields(browser, base_url):
+    URL = base_url + ADMIN_LOGIN_PAGE
+    find_element_after_click(browser, URL, AdminLoginPage.LOGIN_BUTTON, AdminLoginPage.NO_MATCHES_ALERT)
