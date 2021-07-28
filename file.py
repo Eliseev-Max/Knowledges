18b1db10289b47146e9b02729d1f269ef10236a7
Eliseev-Max <m.eliseev@prosoftsystems.ru>
2021-07-28 19:37:05 +0500

    Правки в ДЗ по лог-парсингу

--- file.py
+++ file.py
@@ -3,25 +3,21 @@
 import argparse
 import os.path
 
+# Исправить косяки:
+# Если не добавить \ или/ в конец директории, при вводе файла имена складываются и появляется ошибка
+# Либо пофиксить, либо убрать функционал
+
+# Добавить параметр - количество выводимых строк
 parser = argparse.ArgumentParser(description='Find and open file')
 parser.add_argument('--path', '-p',
                     action='store',
                     help='Enter file path or filename')
-# parser.add_argument('--first',
-#                     dest="a",
-#                     type=int,
-#                     action='store',
-#                     help='Enter file path or filename')
-# parser.add_argument('--second',
-#                     dest="b",
-#                     action='store',
-#                     type=int,
-#                     help='Enter file path or filename')
 
 args = parser.parse_args()
 
 # path = 'C:\\Users\\m.eliseev\\Knowledges\\mini_access.log'
 # this_path = "~/Knowledges/mini_access.log"
+
 
 def prepare_file_to_read(path):
     file = os.path.normpath(path)
@@ -36,7 +32,7 @@ def prepare_file_to_read(path):
         if os.path.exists(full_name):
             return full_name
     else:
-        return print("Указанный файл или каталог не найден")
+        print("Указанный файл или каталог не найден")
 
 
 # print(type(args),type(args.path),sep="\n")
@@ -44,7 +40,10 @@ print(args.path)
 # print(os.path.normpath(args))
 # os.path.expanduser(args)
 # print(os.path.abspath(args))
-
+idx = 0
 with open(prepare_file_to_read(args.path), "r", encoding="utf-8") as f:
     for line in f:
         print(line)
+        if idx>10:
+            break
+        idx+= 1
