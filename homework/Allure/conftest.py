039734378b15c83ae0954abd98b8381d73108f7c
Eliseev-Max <m.eliseev@prosoftsystems.ru>
2021-09-20 20:19:01 +0500

    Отчёт Allure составляется. Осталось прикрутить Selenoid + screenshot

--- homework/Allure/conftest.py
+++ homework/Allure/conftest.py
@@ -16,8 +16,8 @@ def pytest_addoption(parser):
     parser.addoption("--headless", action="store_true", help="Run headless")
     parser.addoption("--browser", action="store", choices=["chrome", "firefox", "opera"], default="chrome")
     # parser.addoption("--executor", action="store", default="10.0.2.15"
-    parser.addoption("--url", action="store", default="http://172.19.16.229/")
-    # parser.addoption("--url", action="store", default="http://10.0.2.15/")
+    # parser.addoption("--url", action="store", default="http://172.19.16.229/")
+    parser.addoption("--url", action="store", default="http://10.0.2.15/")
     # parser.addoption("--url", action="store", default="http://192.168.1.48/")
     parser.addoption("--bversion", action="store", default="88.0")
     parser.addoption("--vnc", action="store_true", default="False")
@@ -26,24 +26,10 @@ def pytest_addoption(parser):
     parser.addoption("--mobile", action="store_true")
 
 
-# @pytest.fixture(scope="session")
-# def get_environment(pytestconfig):
-#     props = {
-#         'Shell': os.getenv('SHELL'),
-#         'Terminal': os.getenv('TERM'),
-#         'Stand': 'Testing'
-#     }
-#
-#     tests_root = pytestconfig.rootdir
-#     with open(f'{tests_root}/allure-results/environment.properties', 'w') as f:
-#         for k, v in props.items():
-#             f.write(f'{k}={v}\n')
-
-
 @pytest.fixture
 def browser(request):
     browser = request.config.getoption("--browser")
-    executor = request.config.getoption("--executor")  # где находится Selenium Server
+    # executor = request.config.getoption("--executor")  # где находится Selenium Server
     headless = request.config.getoption("--headless")
     maximized = request.config.getoption("--maximized")
     version = request.config.getoption("--bversion")
@@ -54,7 +40,7 @@ def browser(request):
     logger = logging.getLogger('BrowserLogger')
     test_name = request.node.name
 
-    executor_url = f"http://{executor}:4444/wd/hub"
+    # executor_url = f"http://{executor}:4444/wd/hub"
     logger.info("\tTest \"{}\" is running".format(test_name))
 
     if browser == "chrome":
@@ -83,6 +69,18 @@ def browser(request):
 
     def final():
         logger.info("\tTest \"{}\" completed\n".format(test_name))
+
+        props = {
+            'Shell': os.getenv('SHELL'),
+            'Terminal': os.getenv('TERM'),
+            'Stand': 'Testing'
+        }
+
+        tests_root = request.config.rootdir
+        with open(f'{tests_root}/allure-results/environment.properties', 'w') as f:
+            for k, v in props.items():
+                f.write(f'{k}={v}\n')
+
         driver.quit()
 
     request.addfinalizer(final)
