c6fb0b95990a5a5d7e8b948528929c43231b0f74
Eliseev-Max <m.eliseev@prosoftsystems.ru>
2021-09-27 19:27:00 +0500

    Начал лекцию по Jenkins

--- homework/Allure/Product.py
+++
@@ -1,9 +0,0 @@
-from Locators import Locators
-
-
-class Product(Locators):
-
-    def go_to_product_page(self, url):
-        self.logger.info("Opening product page, URL: {}".format(url + self.LOCATION_OF_PRODUCT))
-        self.browser.get(url + self.LOCATION_OF_PRODUCT)
-        return self
