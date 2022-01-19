7eff4858a512d284ed1c6af3c1701ad4eeaa5be4
Eliseev-Max <m.eliseev@prosoftsystems.ru>
2022-01-19 19:07:10 +0500

    2022.01.19 Changes

--- homework/LoggingSelenoidAllure/Project/Catalog.py
+++
@@ -1,9 +0,0 @@
-import BaseClass
-
-
-class Catalog(BaseClass.BaseClass):
-
-    def go_to_catalog(self, url):
-        self.logger.info("Opening source: {}".format(url + self.LOCATION_OF_CATALOG))
-        self.browser.get(url + self.LOCATION_OF_CATALOG)
-        return self
